7157a335f9fb004a1af09dcf241a1f81faaceda2
brijuliet <brianna.juliet@gmail.com>
2022-02-01 19:33:42 -0700

    Algorithm Commit 2

---
+++ Sandbox/src/main/java/org/example/Liskov.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Liskov {
+
+    public static void main(String[] args) {
+
+        List<String> stuff = new ArrayList<>();
+
+        Double value = new Double(1.0);
+    }
+}
